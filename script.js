b777a9d32d205c5464a9599bce4230c2545521fc
Rohit Anchal <105239963+rohitanchal@users.noreply.github.com>
2025-02-02 12:25:39 -0500

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
-    //Write your code here
-
+    event.preventDefault(); // Prevents the default form submission behavior
+    
+    const form = document.getElementById("form1");
+    const firstName = form.elements["fname"].value; // Get first name
+    const lastName = form.elements["lname"].value; // Get last name
+    
+    alert(firstName + " " + lastName); // Show the full name in an alert
 }
